bd63d7da1c8d58b6ee97161aec796eabb1dd7b98
Dennis2340 <kamaradennis36@gmail.com>
2024-03-18 22:20:11 +0000

    fix: change output name to zkt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,7 +19,7 @@ variable "main_instance_count" {
 }
 
 output "instance_public_ip" {
-  value       = aws_instance.mock_server[0].public_ip
+  value       = aws_instance.zkt_server[0].public_ip
   description = "AWS EC2 instance public IP"
   sensitive   = false
 }
